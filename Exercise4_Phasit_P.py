adcdc797dac8ddad88b7ccba251bb5f7d1d77ba1
FlorkforGithub <Florkphasit@hotmail.com>
2020-12-30 17:14:16 +0700

    Exercise4

---
+++ Exercise4_Phasit_P.py
@@ -0,0 +1,21 @@
+''' เก็บข้อมูลคะแนนสอน 4 วิชา ของนักศึกษา  '''
+''' รายวิชาทั้ง4 '''
+Fe = " Subject : Foundation English"
+Gb = " Subject : General Bussiness"
+Cs = " Subject : Introduction to Computer System"
+Cp = " Subject : Computer Programming"
+
+'''s = student'''
+s_Fe= Fe+':'+ " 10  point  = A Grade"
+s_Gb= Gb+':'+ " 5   point  = B Grade"
+s_Cs= Cs+':'+ " 2.5 point  = C Grade"
+s_Cp= Cp+':'+ " 9   point  = A Grade"
+
+print("------------Student Score------------")
+print(s_Fe)
+print(s_Gb)
+print(s_Cs)
+print(s_Cp)
+
+
+
