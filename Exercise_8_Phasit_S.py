ab61702501009d0dc24276fac906de0438734065
FlorkforGithub <Florkphasit@hotmail.com>
2021-01-01 23:23:29 +0700

    Update Exercise_8_Phasit_S.py
    แก้ไขโค๊ด Login
    จัดเรียงโค๊ดใหม่

--- Exercise_8_Phasit_S.py
+++ Exercise_8_Phasit_S.py
@@ -12,25 +12,24 @@ if UserNameinput == "customer1" and PassWordinput == "shopping01":
     print("1.monitor 2500 THB Per piece")
     print("2.mouse 1000 THB Per piece")
     print("3.keyboard 2500 THB Per piece")
+    ProductSelected=int(input("You must specify the product Number :"))
+    if ProductSelected == 1:
+        print("1: Monitor - >Price : 2500 THB")
+        Amount=int(input("Amount:"))
+        print("Total Cost:", monitor*Amount)
+        print("-------------------Thank for buying------------------------")
+    if ProductSelected == 2:
+        print("2: Mouse -> Price : 1000 THB")
+        Amount = int(input("Amount:"))
+        print("Total Cost:", mouse * Amount)
+        print("-------------------Thank for buying------------------------")
+    if ProductSelected == 3:
+        print("3: Keyboard -> Price : 2500 THB")
+        Amount = int(input("Amount:"))
+        print("Total Cost:", keyboard * Amount)
+        print("-------------------Thank for buying------------------------")
 else:
     print("Username or Password invalid")
-
-ProductSelected=int(input("You must specify the product Number :"))
-if ProductSelected == 1:
-    print("1: Monitor - >Price : 2500 THB")
-    Amount=int(input("Amount:"))
-    print("Total Cost:", monitor*Amount)
-    print("-------------------Thank for buying------------------------")
-if ProductSelected == 2:
-    print("2: Mouse -> Price : 1000 THB")
-    Amount = int(input("Amount:"))
-    print("Total Cost:", mouse * Amount)
-    print("-------------------Thank for buying------------------------")
-if ProductSelected == 3:
-    print("3: Keyboard -> Price : 2500 THB")
-    Amount = int(input("Amount:"))
-    print("Total Cost:", keyboard * Amount)
-    print("-------------------Thank for buying------------------------")
 
 
 
